c1b868aa933aca2ad93190bdfb8d555c32c54540
AdaephonBen <ma18btech11011@iith.ac.in>
2021-05-18 22:43:36 +0530

    Updated mock server
    Signed-off-by: AdaephonBen <ma18btech11011@iith.ac.in>

--- mock-server/index.js
+++ mock-server/index.js
@@ -5,8 +5,19 @@ app.use(bodyParser.json()); // for parsing application/json
 
 const port = 3000;
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 app.post("/check", (req, res) => {
-  res.send("Yes");
+  const random = getRandomInt(0, 2);
+  if (random == 0) {
+    res.send("No");
+  } else {
+    res.send("Yes");
+  }
 });
 
 app.listen(port, () => {
