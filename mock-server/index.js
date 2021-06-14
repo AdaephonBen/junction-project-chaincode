4e5c12161ca5c1e3f2f74c1f68331fc22dd7e92d
AdaephonBen <ma18btech11011@iith.ac.in>
2021-06-14 18:28:57 +0530

    Added don't know logic
    Signed-off-by: AdaephonBen <ma18btech11011@iith.ac.in>

--- mock-server/index.js
+++ mock-server/index.js
@@ -1,5 +1,6 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
+const geolocationUtils = require("geolocation-utils");
 
 app.use(bodyParser.json()); // for parsing application/json
 
@@ -11,11 +12,52 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+const isAngleBetween = (rangeAngle1, rangeAngle2, angle) => {
+  rangeAngle1 -= rangeAngle1;
+  rangeAngle2 -= rangeAngle1;
+  angle -= rangeAngle1;
+  if (rangeAngle1 < 0) {
+    rangeAngle1 += 360;
+  }
+  if (rangeAngle2 < 0) {
+    rangeAngle2 += 360;
+  }
+  if (angle < 0) {
+    angle += 360;
+  }
+  return angle < rangeAngle2;
+};
+
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
+
 app.post("/check", (req, res) => {
-  const random = getRandomInt(0, 3);
-  if (random == 0) {
-    res.send("No");
-  } else if (random == 1) {
+  const { eventLat, eventLong, carLat, carLong, carOrientation } = req.body;
+  const fieldOfView = 135; // Human eye has a horizontal FOV of 135 degrees
+  const farthestDistance = 10; // Assumed to be 10 metres for now
+
+  const eventLocation = geolocationUtils.createLocation(
+    eventLat,
+    eventLong,
+    "LatLon"
+  );
+  const carLocation = geolocationUtils.createLocation(
+    carLat,
+    carLong,
+    "LatLon"
+  );
+  const headingDistance = geolocationUtils.headingDistanceTo(
+    carLocation,
+    eventLocation
+  );
+  const withinFOV = isAngleBetween(
+    mod(carOrientation - fieldOfView / 2, 360),
+    mod(carOrientation + fieldOfView / 2, 360),
+    headingDistance.heading
+  );
+
+  if (headingDistance.distance < farthestDistance && withinFOV) {
     res.send("Yes");
   } else {
     res.send("Don't know");
